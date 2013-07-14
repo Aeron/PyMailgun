6760ae134e377f3f0d8395630329d74d04a869f8
Eugene “Aeron” Glybin <aeron@aeron.cc>
2013-07-14 16:55:59 +0400

    Recipient variables added for Messages. Minor updates.

--- mailgun/__init__.py
+++ mailgun/__init__.py
@@ -16,3 +16,6 @@ class Mailgun(object):
 		self.messages = Messages(api_key, domain)
 		self.lists = MailingLists(api_key)
 		self.campaigns = Campaigns(api_key, domain)
+
+	def __repr__(self):
+		return "%s(%s, %s) instance at %s" % (self.__class__.__name__, self.api_key, self.domain, hex(id(self)))
--- mailgun/messages.py
+++ mailgun/messages.py
@@ -1,11 +1,12 @@
 # coding: utf-8
+import json
 from .api import MailgunAPI
 
 
 class Messages(MailgunAPI):
 	API_NAME = 'messages'
 
-	def create(self, from_address, to, subject, text, html=None, cc=None, bcc=None, attachment=None, inline=None, o=None, h=None, v=None):
+	def create(self, from_address, to, subject, text, html=None, cc=None, bcc=None, attachment=None, inline=None, o=None, h=None, v=None, recipient_variables=None):
 		for param in (to, cc, bcc):
 			if param and isinstance(param, (list, tuple)):
 				param = ", ".join(param)
@@ -15,6 +16,7 @@ class Messages(MailgunAPI):
 			if data[param_name] and isinstance(data[param_name], dict):
 				for k, v in data[param_name].iteritems():
 					data["%s:%s" % (param_name, k)] = v
+		data['recipient-variables'] = json.dumps(data.pop('recipient_variables'))
 		return self._request('post', data=data)
 
 
